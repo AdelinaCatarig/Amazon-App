17b80295025ba5ab547fcc3cd7291126461486fb
Adelaina <adelinabistriceanu03@gmail.com>
2026-05-05 18:06:58 +0300

    added utils folder for shared functions and made checkout js file and html generation

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,13 +1,12 @@
-export const cart = [];
+export const cart = [{
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity:2}
+];
 
 export function addToCart(productId) {
+    
     let matchingItem;
-
-    cart.forEach((cartItem) => {
-        if(cartItem.productId === productId) {
-            matchingItem = cartItem;
-        }
-    });
+    matchingItem = cart.find(itemCart => itemCart.productId === productId);
 
     if(matchingItem) {
         matchingItem.quantity += 1;
@@ -16,4 +15,6 @@ export function addToCart(productId) {
         productId,
         quantity: 1,});
     }
+
+    console.log(cart);
 }
