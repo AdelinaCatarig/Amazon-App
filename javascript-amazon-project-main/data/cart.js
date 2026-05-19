614c48c24c165445ba8deb0bbaa25b6907d25641
Adelaina <adelinabistriceanu03@gmail.com>
2026-05-19 14:01:01 +0300

    Save cart info to local storage and get data from it

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,12 +1,19 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if(!cart) {
+    cart = [{
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity:2}, {
     productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
     quantity:1}
 ];
+}
 
-export function addToCart(productId) {
-    
+function saveToStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+export function addToCart(productId) {   
     let matchingItem;
     matchingItem = cart.find(itemCart => itemCart.productId === productId);
 
@@ -17,10 +24,10 @@ export function addToCart(productId) {
         productId,
         quantity: 1,});
     }
-
-    console.log(cart);
+    saveToStorage();
 }
 
 export function removeFromCart(productId) {
     cart = cart.filter(itemCart => itemCart.productId !== productId);
+    saveToStorage();
 }
